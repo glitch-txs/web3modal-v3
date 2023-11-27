21123b3d964c1556bcf0ae5a07e5341c81c0ee55
Glitch <glitch.txs@gmail.com>
2023-11-27 14:39:47 -0300

    uopdate deps

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,14 +1,12 @@
-import { useWeb3ModalState } from '@web3modal/wagmi/react'
+import { useWeb3Modal } from '@web3modal/wagmi/react'
 
 export default function ConnectButton() {
 
-  const { open, selectedNetworkId } = useWeb3ModalState()
-  console.log(open, selectedNetworkId)
+  const { open } = useWeb3Modal()
 
   return (
     <>
-      {/* <button onClick={() => modal.open()}>Open Connect Modal</button>
-      <button onClick={() => modal.open({ view: 'Networks' })}>Open Network Modal</button> */}
+      <button onClick={() => open()}>Open Modal</button>
       <w3m-button/>
     </>
   )
